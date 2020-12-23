6ee608e631d1d901bba490f307438769d74dcbb3
st0ffregen <stoffregen@mailbox.org>
2020-12-23 22:19:19 +0100

    fix: add not commited files

--- bot/updateStatus.py
+++ bot/updateStatus.py
@@ -4,18 +4,16 @@ import tweepy
 from datetime import datetime
 import sys
 import os
-from tweetText import tweetArray
+from bot import tweetText
 import traceback
 
 
-def updateStatus(api, idFile):
+def updateStatus(api, idFile, tweetArray, index):
     print("update status")
-    response = api.update_status(tweetArray[int(datetime.now().strftime('%d')) - 1])
+    response = api.update_status(tweetArray[index])
     print(str(response))
-    f = open(idFile, "w")
-    f.write(str(response.id))
-    f.close()
-    return 0
+    writeToFile(idFile, str(response.id))
+    return str(response.id)
 
 
 def checkIfTweetShouldBeDeleted(api, tweetId):
@@ -36,8 +34,22 @@ def deleteStatus(api, tweetId, idFile):
     else:
         print("do not delete status")
 
-    f = open(idFile, "w")
-    f.write("")
+    writeToFile(idFile, "")
+    return 0
+
+
+def readInId(fileName):
+    # figure out if we have to update or delete
+    idFile = fileName
+    f = open(idFile, "r")
+    tweetId = f.read()
+    f.close()
+    return tweetId
+
+
+def writeToFile(fileName, text):
+    f = open(fileName, "w")
+    f.write(text)
     f.close()
     return 0
 
@@ -46,22 +58,16 @@ def updateOrDeleteStatus():
     print("---")
     print("starting bot")
     print("utc time now: " + datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
-    idFile = os.environ['ID_FILE']
 
-    # figure out if we have to update or delete
-    f = open(idFile, "r")
-    tweetId = f.read()
-    f.close()
-
-    # get api
-    oauth = OAuth()
-    api = tweepy.API(oauth)
+    api = initApi()
+    fileName = os.environ['ID_FILE']
+    tweetId = readInId(fileName)
 
     try:
         if tweetId == "":
-            updateStatus(api, idFile)
+            updateStatus(api, fileName, tweetText.tweetArray, int(datetime.now().strftime('%d')) - 1)
         else:
-            deleteStatus(api, tweetId, idFile)
+            deleteStatus(api, tweetId, fileName)
 
     except tweepy.error.TweepError as err:
         print(f"error while working with twitter api: {err}")
@@ -73,10 +79,11 @@ def updateOrDeleteStatus():
     return 0
 
 
-def OAuth():
+def initApi():
     auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
     auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
-    return auth
+    api = tweepy.API(auth)
+    return api
 
 
 if __name__ == "__main__":
